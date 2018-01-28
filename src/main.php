73ebb82d1d732fa0917423e241bba6e374b45d0e
Viktor Volkov <megachupakabra007@mail.ru>
2018-01-28 23:53:32 +0300

    made test version

--- src/main.php
+++ src/main.php
@@ -3,6 +3,7 @@
 
 error_reporting(E_ALL);
 
+/*
 require 'vendor/autoload.php';
 
 use chupacabra007\networking\TcpServer;
@@ -18,11 +19,40 @@ class EchoHandler extends StreamRequestHandler
     }
 }
 
-$tcp = new TcpServer("127.0.0.1", 21623, 'EchoHandler');
+$tcp = new TcpServer("127.0.0.1", 21624, 'EchoHandler');
 
 $tcp->serve_forever();
 
+*/
 
+$host = '127.0.0.1';
+$port = 21626;
+
+$socket = socket_create(AF_INET, SOCK_STREAM, 0);
+socket_bind($socket, $host, $port);
+socket_getsockname($socket, $host, $port);
+socket_listen($socket, 5);
+
+$clients = [$socket];
+while(true)
+{
+    $read = $clients;
+    $write = [];
+    $except = [];
+    if (socket_select($read, $write, $except, 0.5) < 1){
+        continue;
+    }
+    if (in_array($socket, $read)) {
+        handle_request($socket);                       
+    }                
+}
+
+function handle_request($socket)
+{
+    $client = socket_accept($socket); 
+    $data = socket_read($client, 1024, PHP_NORMAL_READ); 
+    socket_write($client, $data . "\n"); 
+}
 
 
 
